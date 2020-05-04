c05ebcf39e9c4bc076bf62dc454d6d5f041b3308
bukarin denis <bukarindi@gmail.com>
2020-05-04 14:55:07 +0400

    initial commit

--- alert.py
+++ alert.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import logging
+import sys
 
 from flask import Flask, request
 
 # Enable logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 logger = logging.getLogger(__name__)
 
 # Initial Flask app
 app = Flask(__name__)
 
+# Liviness test
+@app.route('/', methods=['GET'])
+def homepage():
+    return """
+    <h1>Hello heroku. I am alive</h1>
+    """
+# Bot alerter
 @app.route('/alert', methods=['POST'])
 def alert():
     content = request.get_json()
@@ -18,8 +26,8 @@ def alert():
     try:
         import telegram
         # Initial bot by Telegram access token
-        bot = telegram.Bot(token="botToken")
-        chat_id = "-chatID"
+        bot = telegram.Bot(token="828210247:AAFGvnijvBGVaWuR80S682d7cM6bDlt1qqc")
+        chat_id = "-1001332344815"
         from msg import alert_msg_handler
         for alert_json in content['alerts']:
             parse_msg = alert_msg_handler(alert_json)
@@ -37,5 +45,5 @@ def alert():
 
 if __name__ == "__main__":
     # Running server
-    app.run(host='0.0.0.0', port=9165)
+    app.run()
 
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sed -i s/botToken/"$botToken"/ alert.py
-sed -i s/chatID/"$chatID"/ alert.py
-
-gunicorn -w 4 -b 0.0.0.0:9165 alert:app
